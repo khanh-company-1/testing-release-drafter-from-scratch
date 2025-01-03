262825783c54c99fc52d323c52ee5fcb447f24ce
Khanh Nguyen <119989010+ndkhanh-axonivy@users.noreply.github.com>
2025-01-03 17:35:00 +0700

    MARP-5 Add new feat 5 (#8)

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,6 +5,7 @@ public class App {
         System.out.println("Feature 1");
         System.out.println("Feature 2");
         System.out.println("Feature 3");
-        System.out.println("12.0.0");
+        System.out.println("Feature 5");
+        System.out.println("12.0.1");
     }
 }
