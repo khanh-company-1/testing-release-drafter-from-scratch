4777e08488d7549cbd012e5bbfa86e08decc4cdd
Khanh Nguyen <khanh.nguyenduy@axonactive.com>
2025-01-03 17:00:04 +0700

    Update App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -4,6 +4,7 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Feature 1");
         System.out.println("Feature 2");
+        System.out.println("Feature 3");
         System.out.println("12.0.0");
     }
 }
