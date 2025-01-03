7ef9f0fbc62b5cad58b8508c62df960f50319730
Khanh Nguyen <khanh.nguyenduy@axonactive.com>
2025-01-03 17:39:37 +0700

    Update App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,6 +5,7 @@ public class App {
         System.out.println("Feature 1");
         System.out.println("Feature 2");
         System.out.println("Feature 5-10");
+        System.out.println("Feature 5-10");
         System.out.println("10.0.2");
     }
 }
