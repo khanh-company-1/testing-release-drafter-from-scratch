5165b42be4d40064c0884ea16e09d07791a5b9ea
Khanh Nguyen <khanh.nguyenduy@axonactive.com>
2025-01-03 16:41:36 +0700

    MARP-1 Add new feature 1

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,6 +2,7 @@ package com.khanhcompany1.testingreleasedrafterfromscratch;
 
 public class App {
     public static void main(String[] args) {
+        System.out.println("Feature 1");
         System.out.println("10.0.0");
     }
 }
