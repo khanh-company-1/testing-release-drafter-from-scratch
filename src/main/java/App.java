ff4308ead43db71519527d4c185e437ca64ea4d2
Khanh Nguyen <khanh.nguyenduy@axonactive.com>
2025-01-06 01:57:49 +0700

    MARP-7 Add new feature 7

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -6,6 +6,7 @@ public class App {
         System.out.println("Feature 2");
         System.out.println("Feature 3");
         System.out.println("Feature 5");
-        System.out.println("12.0.1");
+        System.out.println("Feature 7");
+        System.out.println("12.0.2");
     }
 }
