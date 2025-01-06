b747c6042fb18bcfc93f9bd5b4b2ba57f1a722f9
Khanh Nguyen <119989010+ndkhanh-axonivy@users.noreply.github.com>
2025-01-06 13:58:43 +0700

    MARP-12 Add feat 12 (#19)

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,6 +8,7 @@ public class App {
         System.out.println("Feature 5-10");
         System.out.println("Feature 8-10");
         System.out.println("Feature 9-10");
-        System.out.println("10.0.4");
+        System.out.println("Feature 12-10");
+        System.out.println("10.0.5");
     }
 }
